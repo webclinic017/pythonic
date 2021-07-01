70465e153cc26d8ba9d3b24deb2e579c6356617b
krazyKoder <sab.sultana@gmail.com>
2021-07-01 08:14:37 -0700

    Thu Jul  1 08:14:37 PDT 2021

--- app_web/logically/updater.py
+++ app_web/logically/updater.py
@@ -2,13 +2,14 @@
 ## This is an updater script to update datasource -> pd pickles in this case 
 
 
-#### List of tasks 
+#### List of tasks [All DONE]
 ##################################################
 #   Update watchlist ->         add column last updated 
 #   Update and Append EOD ->    data 1d, 1h, 5m, 4h: 1d (5 years), 1h (730 days), 5m (60 days) and 4h (downsampled); 
 #                               read from watchlist, last updated values; fetch and append on new timestamps
 #                               split symbols to download list -> bulk download splits and iterate -> symbol extraction -> fix timezones
 #   
+
 
 
 print ('Hello world')
