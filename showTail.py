2b1512968ada8033ed9ecb61717d71f930a5e48f
krazyKoder <sab.sultana@gmail.com>
2021-08-18 14:32:45 -0700

    Wed Aug 18 14:32:45 PDT 2021

--- showTail.py
+++ showTail.py
@@ -42,3 +42,13 @@ print("Last modified: %s" % time.ctime(os.path.getmtime(fname)))
 # print("Created: %s" % time.ctime(os.path.getctime(fname)))
 print ()
 
+
+
+# # test 
+
+# symbols = ['SPT', 'LB', 'SPY', 'MPC', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MAT', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'MRO', 'M', 'MAC', 'MTB', 'LYB', 'L', 'LMT', 'LKQ', 'LNC', 'LLY', 'LEG', 'LH', 'KR', 'KHC', 'KSS', 'KMI', 'MET', 'MTD', 'MGM', 'MCHP', 'NUE', 'NRG', 'NCLH', 'NOC', 'NTRS', 'NSC', 'JWN', 'NI', 'NKE', 'NLSN', 'NEE', 'NWS', 'NWSA', 'NEM', 'NWL', 'NTAP', 'NAVI', 'NOV', 'NDAQ', 'MSI', 'MOS', 'MS', 'MCO', 'MNST', 'MON', 'MDLZ', 'TAP', 'MHK', 'MAA', 'KIM', 'KMB', 'KEY', 'K', 'HPE', 'HES', 'HSY', 'HSIC', 'HP', 'HCA', 'HAS', 'HIG', 'HOG', 'HBI', 'HAL', 'GWW', 'GT', 'GS', 'GPN', 'GILD', 'GPC', 'GM', 'GIS', 'GE', 'GD', 'IT', 'GRMN', 'GPS', 'FCX', 'BEN', 'FBHS', 'FTV', 'F', 'HLT', 'ORLY', 'HOLX']
+
+# import yfinance as yf
+# import pandas as pd
+
+# yf.download(tickers=symbols, interval='1D', period='200d', group_by="Ticker")
