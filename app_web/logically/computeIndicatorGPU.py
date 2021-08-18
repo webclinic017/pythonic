177207d89cb78997611fc6864f82b5268f2ed30d
krazykoder <sab.sultana@gmail.com>
2021-08-18 11:40:59 -0700

    Wed Aug 18 11:40:59 PDT 2021

--- app_web/logically/computeIndicatorGPU.py
+++ app_web/logically/computeIndicatorGPU.py
@@ -7,7 +7,7 @@
 
 import finta as ft
 import numpy as np
-import pandas as pd
+import cudf as pd
 import pandas_ta as ta
 import yfinance as yf
 from time import sleep
@@ -99,18 +99,32 @@ def compute_all (ddr=None, symbols=None, interval=None) :
 
 
 # Sequencial compute 
-def compute_all_seq (ddr=None, symbols=None, verbose=False) :
+def compute_all_seq (ddr=None, symbols=None, interval=None, verbose=False) :
     tic = time.perf_counter()
-    
-    inDict = {}
+
+    global Algoddr
+    Algoddr = {}
     for symbol, dfdata in ddr.items() :
-        inDict[symbol] = compute_indicatorsA(dfdata, 1, 0)    
+        Algoddr[symbol] = compute_indicatorsA(dfdata, 1, 0)    
     
     if verbose: 
         toc = time.perf_counter()
         print (f"Compute Time :: {toc - tic:0.4f} seconds")
-    return inDict
+    return Algoddr
 
+# Sequencial compute 
+def compute_all_seq_GPU (ddr=None, symbols=None, interval=None, verbose=False) :
+    tic = time.perf_counter()
+
+    global Algoddr
+    Algoddr = {}
+    for symbol, dfdata in ddr.items() :
+        Algoddr[symbol] = compute_indicatorsA_GPU(dfdata, symbol, interval)   
+    
+    if verbose: 
+        toc = time.perf_counter()
+        print (f"Compute Time :: {toc - tic:0.4f} seconds")
+    return Algoddr
 
 
 def compute_indicatorsA (df, symbol, interval, verbose=False) : # simulate a high compute or low latency IO process
@@ -257,3 +271,115 @@ def reduce_mem_usage(df):
     print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
     
     return df
+
+
+
+def compute_indicatorsA_GPU (df, symbol, interval, verbose=False) : # simulate a high compute or low latency IO process
+    """ Comprises of common compute items 
+        1. basic indicators calculations
+        2. indicator settings per (stock, timeframe) // not implemented yet
+    """
+    # print (f"Compute {i} started with {k} secs")
+    # if verbose: # measure start time  
+
+    # set cores = 0 # no multiprocessing 
+    df.ta.cores = 0 ; print ("# Cores=", df.ta.cores )
+
+    tic = time.perf_counter()
+
+    #############################   Calculation Start 
+
+    # df.ta.ha(append=True) # heikinashi bars
+
+    # # add HA_Color to signal -1 0 +1 
+    # df['HA_color'] = np.where(df.HA_close > df.HA_open, 1, -1)
+    
+    # # ema21HA = df['EMA_21HA'] = ta.ema( df.HA_close, length=21, append=True)
+    df['EMA9'] = ta.ema(close=df['Close'], length=9)
+    # ema21 = df.ta.ema(length=21, append=True)
+    # ema42 = df.ta.ema(length=42, append=True)
+    # ema50 = df.ta.ema(length=50, append=True)
+    # ema100 = df.ta.ema(length=100, append=True)
+    # ema150 = df.ta.ema(length=150, append=True)
+    # keltner = df.ta.kc(append=True)
+    # squeezes = df.ta.squeeze(lazybear=False, detailed=True, append=True) 
+    # # squeezes = df.ta.squeeze(lazybear=True, detailed=True, append=True) # _LB
+    # bollingers = df.ta.bbands(append=True)
+
+    # df['signal_SQ2gauss']= ft.TA.SQZMI(df).apply(lambda x: -1 if x else 0)
+    # df['signal_trTTM'] = df.ta.ttm_trend(append=True)
+
+    # # df['signal_decreasing'] = df.ta.decreasing(length=8).apply(lambda x: -1 if x==1 else x)
+    # # df['signal_increasing'] =df.ta.increasing(length=8)
+    # # length = 3
+    # # df['signal_IDClose'] = df.ta.decreasing(close=df.HA_close, length=length, strict=True).apply(lambda x: -1 if x==1 else x) + df.ta.increasing(close=df.HA_close,length=length, strict=True)
+    # # df['signal_IDhigh'] = df.ta.decreasing(close=df.HA_high, length=length, strict=True).apply(lambda x: -1 if x==1 else x) + df.ta.increasing(close=df.HA_high,length=length, strict=True)
+
+    # # Exit Chandelier (based on FINTA)
+    # chandelier = ft.TA.CHANDELIER(df, long_period=15, short_period=15)
+    # df['chxLong'], df['chxShort'] = chandelier['Long.'], chandelier['Short.']
+    # df['chxLong'] = np.where(df['chxLong'] >= df['Close'], np.NaN, df['chxLong'])
+    # df['chxShort']= np.where(df['chxShort'] < df['Close'], np.NaN, df['chxShort'])
+    # df['signal_sChandelier'] = np.where(df['chxShort'] > 0, -1, 1) # first hint of red
+    # # df['signal_chandelierL'] = np.where(df['chxLong'] > 0, 1, -1)
+
+
+    # # New Squeeze (black dots) : 1 = ON, 0 = OFF (for 'in_squeeze')
+    # def in_squeeze(df):
+    #     if df['BBL_5_2.0'] > df['KCLe_20_2'] and df['BBU_5_2.0'] < df['KCUe_20_2'] :
+    #         return 1
+    # def out_squeeze(df):
+    #     if not (df['BBL_5_2.0'] > df['KCLe_20_2'] and df['BBU_5_2.0'] < df['KCUe_20_2']):
+    #         return 1
+    # df['squeeze_on'] = df.apply(in_squeeze, axis=1)
+    # df['squeeze_off'] = df.apply(out_squeeze, axis=1)
+
+    # # PSAR Stop Reverse (based on pandas_TA)
+    # psar = df.ta.psar( append=True)
+    # # print (df)
+
+    # # add HA_Color to signal -1 0 +1 
+    # # df['signal_trHAcolor'] = df['HA_color']
+
+    # # define stack EMA Extreme:base=9 ::  +1: positive bullish, 0:undecided, -1:negative bearish
+    # df['signal_StackEMA'] = np.where(
+    #     (df.EMA_9 > df.EMA_21) & 
+    #     (df.EMA_21 > df.EMA_50) & 
+    #     (df.EMA_50 > df.EMA_100) &
+    #     (df.EMA_100 > df.EMA_150)      
+    #     ,1, np.where(
+    #     (df.EMA_9 < df.EMA_21) & 
+    #     (df.EMA_21 < df.EMA_50) & 
+    #     (df.EMA_50 < df.EMA_100) &
+    #     (df.EMA_100 < df.EMA_150), -1, 0))
+
+    # # define stack EMA Softer:base=21 :: +1: positive bullish, 0:undecided, -1:negative bearish
+    # df['signal_StackEMA21'] = np.where(
+    #     # (df.EMA_9 > df.EMA_21) & 
+    #     (df.EMA_21 > df.EMA_50) & 
+    #     (df.EMA_50 > df.EMA_100) &
+    #     (df.EMA_100 > df.EMA_150)      
+    #     ,1, np.where(
+    #     # (df.EMA_9 < df.EMA_21) & 
+    #     (df.EMA_21 < df.EMA_50) & 
+    #     (df.EMA_50 < df.EMA_100) &
+    #     (df.EMA_100 < df.EMA_150), -1, 0))
+
+    # # reformat Squeese (Original) for Charting 
+    # df['SQZ'] = df['SQZ_ON']    # final SQZ market
+    # # df['SQZ_Hist'] = df['SQZ_20_2.0_20_1.5_LB'] # final SQZ Hist
+    # df['SQZ_Hist'] = df['SQZ_20_2.0_20_1.5'] # final SQZ Hist
+    # # mark increasing decreasing : 2 inc blue, 1 dec deep blue, -1 red dec, -2 yellow inc
+    # df['SQZ_HistC'] = np.where(df['SQZ_PINC'].notnull(), 2, np.nan)
+    # df['SQZ_HistC'] = np.where(df['SQZ_PDEC'].notnull(), 1, df['SQZ_HistC'])
+    # df['SQZ_HistC'] = np.where(df['SQZ_NDEC'].notnull(), -1, df['SQZ_HistC'])
+    # df['SQZ_HistC'] = np.where(df['SQZ_NINC'].notnull(), -2, df['SQZ_HistC'])
+
+    # #############################   Calculation END 
+
+    if verbose: 
+        toc = time.perf_counter()
+        print (f"Compute Time :: {toc - tic:0.4f} seconds")
+        print (f"Compute {symbol} done with {interval} interval.")
+    
+    return (df, symbol, interval)
