4554206625a70ba17d4fc58e894eed85f4e3b1af
krazyKoder <sab.sultana@gmail.com>
2021-08-30 12:57:58 -0700

    Mon Aug 30 12:57:58 PDT 2021

--- app_web/logically/EODUpdater.py
+++ app_web/logically/EODUpdater.py
@@ -1,6 +1,6 @@
 # This script is a blank updater for database
 # Update intervals : 5m, 1H, 1D , 4H (resampled)
-# Uses: df reduce_mem_usage before persist to disk - no active right now 
+# Uses: df reduce_mem_usage before persist to disk - no active right now
 
 
 import yfinance as yf
@@ -8,21 +8,24 @@ import datasource as data
 
 #####################Yahoo finance API TEST ######
 
-# test yf libs => functional 
+# test yf libs => functional
 print(yf.download(tickers='SPY', interval='1h', period='2d'))  ## check additional
 print(yf.download(tickers='SPY', interval='1WK', period='1d').tail(8)) ## check additional
 print(yf.download(tickers='SPY', interval='5m', period='1d').tail(8))  ## check additional
 
 
-### First Update all watchlist 
+### First Update all watchlist
 data.updateWatchlistLastUpdated(watchlistName='WatchListDBFull.pickle')
 data.updateWatchlistLastUpdated(watchlistName='WatchListLive.pickle')
-data.updateWatchlistLastUpdated() # DEFAULT watchlist 
+data.updateWatchlistLastUpdated() # DEFAULT watchlist
 
 ##############################
+## Defaults
+# chunksize=25, persist=False
+
 data.updateDataEODAll(watchlistName='WatchListLive.pickle', chunksize=25, persist=True) #default chunksize=25
 
-data.updateDataEODAll(chunksize=100, persist=True) # default watchlist 
+data.updateDataEODAll(chunksize=100, persist=True) # default watchlist
 
 data.updateDataEODAll(watchlistName='WatchListDBFull.pickle', chunksize=100, persist=True) ## update all database
 
@@ -30,6 +33,6 @@ data.updateDataEODAll(watchlistName='WatchListDBFull.pickle', chunksize=100, per
 
 #################################
 
-## Test 
+## Test
 # data.getWatchlist(watchlistName='delistedWatchList.pickle')
 #################################
